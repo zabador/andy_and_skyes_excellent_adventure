89ce037baf96e6a9e94172f3d13e1f42de899b39
Skye <snschnei@oakland.edu>
2013-08-20 16:30:19 -0400

    added Entity.java

---
+++ src/com/zabador/entities/Entity.java
@@ -0,0 +1,21 @@
+package com.zabador.entities;
+
+import java.awt.Graphics2D;
+
+public class Entity {
+
+    int x, y;
+
+    public Entity(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update() {
+
+    }
+
+    public void draw(Graphics2D g2d) {
+
+    }
+}
