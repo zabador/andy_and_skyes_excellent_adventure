1c3a6b61e0d8cafcf83bc3b35dbf21dc181a22a8
Skye <snschnei@oakland.edu>
2013-08-22 08:48:51 -0400

    program is no longer an applet

--- src/com/zabador/graphics_engine/GameFrame.java
+++ src/com/zabador/graphics_engine/GameFrame.java
@@ -1,60 +1,77 @@
 package com.zabador.graphics_engine;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.*;
+import java.net.*;
+import javax.swing.*;
 
-import java.util.Timer;
-
-import javax.swing.JPanel;
-
-import com.zabador.entities.Hero;
-
-public class GameFrame extends JPanel implements Runnable, ActionListener{
-
-    Thread gameLoop;
-    Hero hero;
+@SuppressWarnings("serial")
+public class GameFrame extends JFrame implements Runnable, KeyListener {
+    Image image;
+    Thread gameloop;
+    Random rand = new Random();
 
     public GameFrame() {
-        setFocusable(true);
+        super("Our game");
+        setSize(800, 600);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        hero = new Hero(100,100);
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        image = tk.getImage(getURL("/images/player.png"));
+
+        gameloop = new Thread(this);
+        gameloop.start();
     }
 
-    public void paint(Graphics g) {
-        super.paint(g);
-
-        Graphics2D g2d = (Graphics2D)g;
-        hero.draw(g2d);
-
-    }
-
-    public void start() {
-        gameLoop = new Thread(this);
-        gameLoop.start();
+    private URL getURL(String filename) {
+        URL url = null;
+        try {
+            url = this.getClass().getResource(filename);
+        } catch (Exception e) {
+        }
+        return url;
     }
 
     public void run() {
-        //acquire the current thread
         Thread t = Thread.currentThread();
-        //keep going as long as the thread is alive
-        while (t == gameLoop) {
+        while (t == gameloop) {
             try {
-                //target framerate is 50 fps
                 Thread.sleep(20);
-            }
-            catch(InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             repaint();
         }
     }
 
+    public void update(Graphics g) {
+        paint(g);
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(image, 100, 100, this);
+    }
+
     @Override
-    public void actionPerformed(ActionEvent arg0) {
-        System.out.println("key pressed");
-        hero.update();
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
 }
